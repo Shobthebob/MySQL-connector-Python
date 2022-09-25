0d88631893d67fad19eaa814d2bfae4b9949fb18
Shoubhit <77617762+Shobthebob@users.noreply.github.com>
2022-09-25 12:03:51 +0530

    Update Edit values.py

--- Edit values.py
+++ Edit values.py
@@ -1,11 +1,9 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(host="localhost", user="root" , password="shob25" , database="test")
+mydb = mysql.connector.connect(host="localhost", user="root" , password="" , database="")
 
 mycursor = mydb.cursor( )
-sql = "UPDATE accounts SET Username='SUP BRO' WHERE Passcode='Password!123'"
-# Can also do vice versa 
-# sql = "UPDATE accounts SET Passcode='Never gonna give you up' WHERE Username='Shob'"
+query = "UPDATE accounts SET Username='SUP BRO' WHERE Passcode='Password!123'"
 
-mycursor.execute(sql)
+mycursor.execute(query)
 mydb.commit( )
