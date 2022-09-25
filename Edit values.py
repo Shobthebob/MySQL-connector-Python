c14533e1967861c86cfa82b5922c4219b6956ec0
Shoubhit <77617762+Shobthebob@users.noreply.github.com>
2022-09-25 12:12:56 +0530

    Update Edit values.py

--- Edit values.py
+++ Edit values.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(host="localhost", user="root" , password="" , database="")
 
 mycursor = mydb.cursor( )
-query = "UPDATE accounts SET Username='SUP BRO' WHERE Passcode='Password!123'"
+query = "UPDATE accounts SET <command> WHERE <command>"
 
 mycursor.execute(query)
 mydb.commit( )
